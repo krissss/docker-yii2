245ac9af58322addbf2fe67f6f4752fba1232704
kriss <462679766@qq.com>
2017-11-09 14:39:04 +0800

    feat: basic例子增加debug模式定义

--- example-basic/config/db.php
+++ example-basic/config/db.php
@@ -1,18 +1,4 @@
 <?php
-/**
- * 获取环境变量
- * @param $key
- * @param null $default
- * @return null|string
- */
-function env($key, $default = null)
-{
-    $value = getenv($key);
-    if ($value === false) {
-        return $default;
-    }
-    return $value;
-}
 $dsn = env('DB_DSN', 'mysql:host=127.0.0.1;dbname=yii2basic');
 $username = env('DB_USERNAME', 'test');
 $password = env('DB_PASSWORD', '123456');
---
+++ example-basic/config/global.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 获取环境变量
+ * @param $key
+ * @param null $default
+ * @return null|string
+ */
+function env($key, $default = null)
+{
+    $value = getenv($key);
+    if ($value === false) {
+        return $default;
+    }
+    return $value;
+}
