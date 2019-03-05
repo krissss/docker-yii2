55ca38067bb85201c4b4de99219ad169b8cb42ab
kriss <462679766@qq.com>
2019-03-05 15:25:59 +0800

    fix: 编译时的错误

--- update.php
+++ update.php
@@ -6,23 +6,23 @@ $commonDockerFileReplace = [
     '${USE_MIRROR}' => '#', // 本地测试时使用镜像可以加速 apt-get
     '${PHP_VERSION}' => '{$VERSION}',
     'ARG PHP_VERSION' => '',
-    '${MCRYPT}' => '', // php 7.2 已经完全移除 mcrypt 扩展到 pecl 中。@link http://php.net/manual/zh/migration71.deprecated.php
+    '${MCRYPT}' => 'mcrypt \\', // php 7.2 已经完全移除 mcrypt 扩展到 pecl 中。@link http://php.net/manual/zh/migration71.deprecated.php
     '${SQLSRV_VERSION}' => '5.3.0', // 5.3.0以上版本不支持 php7.0，5.5以上可能才支持 php7.3 @link https://pecl.php.net/package/sqlsrv
     '${NODE_JS_VERSION}' => '10', // 大版本，6、8、10、11
 ];
 $replaceFileData = [
     '7.0' => [
         '/Dockerfile' => array_merge($commonDockerFileReplace, [
-            '${MCRYPT}' => 'mcrypt \\',
         ]),
     ],
     '7.1' => [
         '/Dockerfile' => array_merge($commonDockerFileReplace, [
-            '${MCRYPT}' => 'mcrypt \\',
         ]),
     ],
     '7.2' => [
-        '/Dockerfile' => $commonDockerFileReplace,
+        '/Dockerfile' => array_merge($commonDockerFileReplace, [
+            '${MCRYPT}' => '\\',
+        ]),
     ],
 ];
 
